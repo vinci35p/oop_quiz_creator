c749b4938b60df26b6cfb742538bd6e05815a601
Vincent35p <vinsoyt4321@gmail.com>
2025-05-23 19:31:08 +0800

    now edited the code to be oop, but didn't work, going to fix the error

--- main.py
+++ main.py
@@ -1,62 +1,66 @@
+from colorama import Fore
 from quiz_compiler import QuizCompiler
 from quiz_executor import QuizExecutor
 
-# ---- MAIN QUIZ ----
-file = 'quiz_txt'
+def main():
+    file = 'quiz_text'
 
-quiz_questions = read_txt_file(file)
-
-while True:
-    print(Fore.CYAN + "Welcome to your own quiz creator!\n")
-    while True:
-        user_decision = str(input(Fore.LIGHTBLUE_EX + "Enter '1' if you want to create a new set of questionnaires, '2' if you just want "
-                              "to add questions, and '3' if you don't want to continue: "))
-
-        if user_decision in ['1', '2', '3']:
-            break
-
-        else:
-            print(Fore.RED + "Invalid input. Enter just '1', '2' or '3'.")
-
-    if user_decision == '1':
-        delete_quiz_file(file)
-        compiler_quiz()
-
-    if user_decision == '2':
-        compiler_quiz()
-
-    if user_decision == '3':
-        break
-
-while True:
-    print(Fore.YELLOW + "\nDo you like to take the quiz now?")
+    quiz_questions = QuizExecutor.read_txt_file(file)
 
     while True:
-        user_choice = str(input(Fore.LIGHTRED_EX + "Enter your choice (Y/N): ")).strip().upper()
+        print(Fore.CYAN + "Welcome to your own quiz creator!\n")
+        while True:
+            user_decision = str(input(Fore.LIGHTBLUE_EX + "Enter '1' if you want to create a new set of questionnaires, '2' if you just want "
+                                  "to add questions, and '3' if you don't want to continue: "))
 
-        if user_choice in ['Y','N']:
+            if user_decision in ['1', '2', '3']:
+                break
+
+            else:
+                print(Fore.RED + "Invalid input. Enter just '1', '2' or '3'.")
+
+        if user_decision == '1':
+            QuizExecutor.delete_quiz_file(file)
+            QuizCompiler()
+
+        if user_decision == '2':
+            QuizCompiler()
+
+        if user_decision == '3':
             break
-        else:
-            print(Fore.RED + "Invalid input, enter 'Y' or 'N'.")
 
-    if user_choice == 'Y':
-        quiz_questions = read_txt_file(file)
-        if not quiz_questions:
-            print(Fore.MAGENTA + "No quiz questions found. Please create questions first.")
+    while True:
+        print(Fore.YELLOW + "\nDo you like to take the quiz now?")
+
+        while True:
+            user_choice = str(input(Fore.LIGHTRED_EX + "Enter your choice (Y/N): ")).strip().upper()
+
+            if user_choice in ['Y','N']:
+                break
+            else:
+                print(Fore.RED + "Invalid input, enter 'Y' or 'N'.")
+
+        if user_choice == 'Y':
+            quiz_questions = QuizExecutor.read_txt_file(file)
+            if not quiz_questions:
+                print(Fore.MAGENTA + "No quiz questions found. Please create questions first.")
+                break
+            else:
+                retry = 'Y'
+                while retry == 'Y':
+                    QuizExecutor.execute_quiz(quiz_questions)
+                    while True:
+                        retry = str(input(Fore.GREEN + "Do you want to take the quiz again? (Y/N): "))
+                        if retry in ['Y', 'N']:
+                            break
+                        else:
+                            print(Fore.RED + "Invalid input. Enter 'Y' or 'N'.")
+                print(Fore.GREEN + "Returning to main query.")
+
+
+        if user_choice == 'N':
+            print(Fore.LIGHTGREEN_EX + "\nExiting, have a nice day!")
             break
-        else:
-            retry = 'Y'
-            while retry == 'Y':
-                execute_quiz(quiz_questions)
-                while True:
-                    retry = str(input(Fore.GREEN + "Do you want to take the quiz again? (Y/N): "))
-                    if retry in ['Y', 'N']:
-                        break
-                    else:
-                        print(Fore.RED + "Invalid input. Enter 'Y' or 'N'.")
-            print(Fore.GREEN + "Returning to main query.")
 
-
-    if user_choice == 'N':
-        print(Fore.LIGHTGREEN_EX + "\nExiting, have a nice day!")
-        break
+if __name__ == "__main__":
+    main()
